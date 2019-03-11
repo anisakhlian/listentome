777b6a6319ca844696698e04bb95137362f73022
Sakhlyan <you@example.com>
2019-03-11 13:14:15 +0400

    runtime changed

--- listentome/wsgi.py
+++ listentome/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for listentome project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
 
 import dotenv
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
@@ -22,3 +14,4 @@ elif os.environ.get('IS_DEVELOPMENT') == 'True':
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'listentome.settings.' + settings_file)
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
